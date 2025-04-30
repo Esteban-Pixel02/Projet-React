8efb7b55f07162c1f771c25e050b21d7b4598321
Esteban <esteban.barbosa.2001@gmail.com>
2025-04-30 19:49:38 +0200

    Setup app

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Accueil from './Pages/Home/Home.jsx';
 import Propos from './Pages/About/About.jsx';
 import Error from './Pages/Error/Error.jsx'; 
-import House from './Pages/House/House.jsx'
-import './assets/Style/App.scss'
-
+import House from './Pages/House/House.jsx';
+import './assets/Style/App.scss';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Projet-React">  {/* Ajout du basename */}
       <Routes>
         <Route index element={<Accueil />} />
         <Route path="/propos" element={<Propos />} />
         <Route path="/house/:id" element={<House />} />
-        <Route path="*" element={<Error />} /> 
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
