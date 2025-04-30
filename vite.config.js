bde7ad61ca3b4b88b2be42544e01aa514fec4e1f
Esteban <esteban.barbosa.2001@gmail.com>
2025-04-30 19:45:31 +0200

    Setup Pages gitHub

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // 👈 important pour que les chemins fonctionnent après build
+  base: '/Projet-React/', // 👈 important pour que les chemins fonctionnent après build
   plugins: [react()],
 })
