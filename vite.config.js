bd3fb2d66840d6b994d7a74e275ff39226ba2cae
Esteban <esteban.barbosa.2001@gmail.com>
2025-04-30 19:35:50 +0200

    Change syntaxe mixins

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 important pour que les chemins fonctionnent après build
   plugins: [react()],
 })
